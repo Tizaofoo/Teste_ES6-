377355f876749ce0ac4f9d6d743cfd80d89df746
Tiago Troquato de Oliveira <158060634+Tizaofoo@users.noreply.github.com>
2024-07-02 22:30:46 -0300

    corrigido

--- src/media.js
+++ src/media.js
@@ -1,36 +1,17 @@
-function Aluno(nome, qtfaltas, nota) {
-    this.nome = nome;
-    this.qtfaltas = qtfaltas;
-    this.nota = nota;
-}
+// Array de objetos com propriedades 'nome' e 'nota'
+const alunos = [
+    { nome: 'João', nota: 7 },
+    { nome: 'Maria', nota: 5 },
+    { nome: 'Pedro', nota: 8 },
+    { nome: 'Ana', nota: 4 },
+    { nome: 'Carlos', nota: 6 },
+];
 
-    let Aluno1 = new Aluno("Jose Silva", 0, [0,10,20]);
-    let Aluno2 = new Aluno("Paulo Santos", 5, [0,1,2]);
-    let Aluno3 = new Aluno("Jose Santos", 10, [0,5,8]);
-    let Aluno4 = new Aluno("Antonio Silva", 4, [0,7,9]);
-    let Aluno5 = new Aluno("Maria dos santos", 6, [0,3,4]);
+// Função para filtrar notas maiores ou iguais a 6
+const filtrarNotasAprovadas = (alunos) => {
+    return alunos.filter(aluno => aluno.nota >= 6);
+};
 
-    let listalunos = [Aluno1 , Aluno2, Aluno3, Aluno4, Aluno5]
-
-    console.log(listalunos)
-
-
-  // trete
-
-    const acum = [];
-
-    listalunos.forEach(aluno => {
-    const match = acum.find(media => media.nome === aluno.nome);
-    if (match) {
-        match.soma += aluno.nota
-        match.notas++;
-    } else {
-        aluno.soma = aluno.nota
-        aluno.notas = 1;
-        acum.push(aluno)
-    }
-});
-
-    const medias = [].map.call(acum, i => ({nome: i.nome, media: i.soma / i.notas}))
-
-    console.log(medias);
+// Exibição dos alunos aprovados no console
+console.log('Alunos aprovados:');
+console.log(filtrarNotasAprovadas(alunos));
